8d6f7fa428efc25e8ecde27c7593e8f5019665dc
santatramaximin <46313490+santatramaximin@users.noreply.github.com>
2019-01-10 20:34:40 +0300

    Final
    Final

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,49 +1,86 @@
-def lancer
-	return rand(6)
+def full_pyramide(depl)
+	etage=10
+	x=[]
+#compteur de boucles
+i=1
+#nombre d'espace
+space= " "
+#incrementation de espace
+î=etage
+a=1
+position=depl
+while (i<=etage) do
+	if i==position
+ x <<  ((space * (î-1))+("G")+("#" * a ))
+else
+	x <<  ((space * (î))+("#" * a ))
 end
-
-puts "Welcome challenger, I'm sure you're ready to be in level 10 so...\n Let START"
-puts "Press any key to Start"
-gets
-choix = ""
-	niv = 0
-	win = 0
-	while niv != 10 do
-		rep = nil
-		puts " (Any key)Relancer \n (Q)Quitter \n"
-		rep = gets.chomp.upcase
-		system "clear"
-		break if rep == 'Q'
-		if lancer == 5 || lancer == 6
-			puts "Vous avez avance d'une marche"
-			niv += 1
-		elsif lancer == 1 
-			puts "Vous avez recule d'une marche"
-			niv -= 1
-		else
-			puts "Vous n'avez pas bouger"
-		end
-	
-		puts "Vous vous trouve au niveau #{niv}"
-				m = 10
-					for i in 1..10
-						j = 2*i-1
-						if i != m
-							puts "#{' '*(10-i)}#{'#'*j}"
-						else
-							puts "#{' '*(10-i)}p#{'#'*j}"
-						end
-					end
-
-		break if rep == 'Q'
-		win += 1 if niv == 10
-	end
+	i+=1
+	î-=1
+	a+=2
+end
+puts x
+end	
+def lancer
+    puts "lancer le de ? ( y / n )"
+    rep=gets.chomp
 
 
-	if win >= 1
-		puts "Felicitaion! Vous avez Reussi!"
-	else
-		puts "Try again?"
-	end
-	
- 
+    if rep== 'y'
+    de=1+rand(6)
+    
+    end
+    return de    
+end
+            
+
+
+def action
+	depl=10
+	while depl>0
+
+    de=lancer
+
+
+   if de==5
+        depl-=1
+        puts de
+        full_pyramide(depl)
+        puts "avance d'une marche"
+
+    elsif de==6
+        depl-=1
+        puts de
+        full_pyramide(depl)
+        puts "vous avez montez"
+
+    elsif de==1
+        if depl<10
+        	depl+=1
+        puts de
+        full_pyramide(depl)
+        puts"descend d'une case"
+    else
+    	full_pyramide(depl)
+        end
+    elsif de==2
+    	puts de
+    	full_pyramide(depl)
+    	puts "rien ne se passe"
+    elsif de==3
+    	puts de
+    	full_pyramide(depl)
+    	puts "rien ne se passe"
+    elsif de==4  
+    puts de 
+    full_pyramide(depl)
+    puts "rien ne se passe"
+end
+end
+if depl==0
+	puts "-------------------"
+	puts " vous-avez gagne!!"
+	puts "-------------------"
+end
+end
+action
